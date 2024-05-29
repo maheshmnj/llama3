564994774c2babc79a05a970415a6de3b68acde4
Mahesh H Jamdade <mjamdade@umassd.edu>
2024-05-29 17:23:59 -0400

    minor download script changes

--- download.sh
+++ download.sh
@@ -16,8 +16,8 @@ if [[ $MODEL_SIZE == "" ]]; then
 fi
 
 echo "Downloading LICENSE and Acceptable Usage Policy"
-curl -C - -o ${TARGET_FOLDER}"/LICENSE" ${PRESIGNED_URL/'*'/"LICENSE"}
-curl -C - -o ${TARGET_FOLDER}"/USE_POLICY" ${PRESIGNED_URL/'*'/"USE_POLICY"}
+wget --continue ${PRESIGNED_URL/'*'/"LICENSE"} -O ${TARGET_FOLDER}"/LICENSE"
+wget --continue ${PRESIGNED_URL/'*'/"USE_POLICY"} -O ${TARGET_FOLDER}"/USE_POLICY"
 
 for m in ${MODEL_SIZE//,/ }
 do
@@ -44,12 +44,12 @@ do
 
     for s in $(seq -f "0%g" 0 ${SHARD})
     do
-        curl -C - -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"}
+        wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/consolidated.${s}.pth"
     done
 
-    curl -C - -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"}
-    curl -C - -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"}
-    curl -C - -o ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk" ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"}
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/params.json"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/params.json"
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/tokenizer.model"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/tokenizer.model"
+    wget --continue ${PRESIGNED_URL/'*'/"${MODEL_PATH}/checklist.chk"} -O ${TARGET_FOLDER}"/${MODEL_FOLDER_PATH}/checklist.chk"
     echo "Checking checksums"
     CPU_ARCH=$(uname -m)
     if [[ "$CPU_ARCH" == "arm64" ]]; then
